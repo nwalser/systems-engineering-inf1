96d147db91c35e15e3c3d684be8c53507c2e8249
Nathaniel Walser <nathaniel.walser@esp-engineering.com>
2022-10-06 10:30:27 +0200

    added week 3 and fixed some bugs

--- week2/INF2_3.c
+++ week2/INF2_3.c
@@ -4,10 +4,10 @@
 #include <stdio.h>
 int main (int argc, char** argv) {
     double celcius, fahrenheit;
-    // Eingabe in Fahrenheit
+    // Eingabe in Celsius
     printf("Temperatur in Celsius eingeben: ");
     scanf("%lf", &celcius);
-    // Ausgabe in Celcius
+    // Ausgabe in Fahrenheit
     fahrenheit = celcius * 9.0 / 5.0 + 32.0;
     printf("Temperatur in Fahrenheit: %lf\n", fahrenheit);
     return 0;
